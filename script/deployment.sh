2c14973bae85511fa145f14beda946af1ccc38f3
alianjum <m.alianjum0@gmail.com>
2020-10-22 19:55:48 +0200

    docker creation added

--- script/deployment.sh
+++ script/deployment.sh
@@ -2,6 +2,6 @@
 set -e
 echo $DOCKER_PASSWORD | docker login --username "$DOCKER_USERNAME" --password-stdin
 ./gradlew -p ./monolithic/ui packageToContainer
-docker tag zutherb/ci_theapp:latest $DOCKER_USERNAME/ci_theapp:latest
+docker tag zutherb/monolithic-shop:latest $DOCKER_USERNAME/ci_theapp:latest
 docker push $DOCKER_USERNAME/ci_theapp:latest
 
